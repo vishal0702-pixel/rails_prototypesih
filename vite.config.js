ebb4cbb01eab8b6774d5b15cf482743bfcea92b3
vishal k <vishalcodex07@gmail.com>
2025-09-09 10:39:45 +0530

    Initial commit for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rails_prototypesih/', // <--- ADD THIS
+  base: '/railsSIH/', // <--- ADD THIS
 })
